0df153097b758db306c4475bfb20a174c9057f4d
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-06-30 21:35:18 +0530

    Moved code to function.

--- py_pageres/main.py
+++ py_pageres/main.py
@@ -1,4 +1,7 @@
 from ghost import Ghost
-ghost = Ghost()
-page, resources = ghost.open('http://google.com')
-ghost.capture_to('header.png', selector="body")
+
+
+def main():
+    ghost = Ghost()
+    page, resources = ghost.open('http://google.com')
+    ghost.capture_to('header.png', selector="body")
--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 import sys
 
-setup(name = 'py-pageres',
-      description = 'Python tool to Capture Web Screenshots',
-      version = '1.0.1',
-      license = 'GPL v3.0',
-      author = 'Sriram Sundarraj',
-      author_email = 'ssundarraj@gmail.com',
-      packages = ['py_pageres'],
-      entry_points = {
+setup(name='py-pageres',
+      description='Python tool to Capture Web Screenshots',
+      version='1.0.1',
+      license='GPL v3.0',
+      author='Sriram Sundarraj',
+      author_email='ssundarraj@gmail.com',
+      packages=['py_pageres'],
+      entry_points={
           'console_scripts': ['py-pageres=py_pageres:main'],
       },
-      install_requires = [      
-               'pyside',
-               'Ghost.py'
+      install_requires=[
+          'pyside',
+          'Ghost.py'
       ],
-      url = 'https://github.com/ssundarraj/py-pageres',
-      keywords = ['screenshot', 'webpage screenshot', 'python'],
-      classifiers = [
+      url='https://github.com/ssundarraj/py-pageres',
+      keywords=['screenshot', 'webpage screenshot', 'python'],
+      classifiers=[
           'Operating System :: POSIX',
           'Environment :: Console',
           'License :: OSI Approved :: MIT License',
@@ -27,7 +27,7 @@ setup(name = 'py-pageres',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
-          'Topic :: Utilities', 
+          'Topic :: Utilities',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       )
